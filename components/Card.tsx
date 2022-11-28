4330e542121bf4f3226b2cd07f04de9985827ad7
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2022-11-28 21:57:21 +0200

    [ADD] Cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import card from "../styles/Arc.Card.module.scss";
 import { ICard } from "../types/IArchive";
 
+import { TbClock, TbLocation } from "react-icons/tb";
+
 const Card = ({
   name,
   shortDescription,
@@ -15,20 +17,25 @@ const Card = ({
     <div className={card[classNames]}>
       {/* <Image src={logo} alt={name} width={16} height={5} layout="responsive"/> */}
       <div className={card.title} dangerouslySetInnerHTML={{ __html: name }} />
-      <div className="card-description">{shortDescription}</div>
-      <div className="card-date">{date}</div>
-      <div className="card-location">{location}</div>
-      <div className="card-background">
+      <div className={card.desc}>{shortDescription}</div>
+      <div className={card.info}>
+        <div className={card.card_date}>
+          <TbClock />
+          {date}
+        </div>
+        <div className={card.card_location}>
+          <TbLocation />
+          {location}
+        </div>
+      </div>
+      <div className={card.bg}>
         <Image
           src={backgroundImg}
-          alt={name}
-          width={16}
-          height={5}
-          layout="responsive"
+          alt={shortDescription}
+          fill={true}
         />
-      
-    </div>
-    
+        <div className={card.overlay} />
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,48 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import CardContainer from "../partials/Arc/CardsContainer";
+import { METADATA, TITLE } from "../constants/arc";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div
-      className={styles.container}
-      style={{ height: "100vh", width: "100vw" }}
-    >
+    <div className={styles.container}>
       <Head>
-        <title>HackTUES 9</title>
-        <meta name="description" content="HackTUES 9" />
-        <link rel="icon" href="/favicon.png" />
+        <title>{METADATA.title}</title>
+
+        <meta name="charset" content="utf-8" />
+
+        <meta name="title" content={METADATA.title} />
+        <meta name="description" content={METADATA.description} />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="theme-color" content={METADATA.themeColor} />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:type" content="website" />
+        <meta name="twitter:site" content={METADATA.twitter} />
+        <meta name="twitter:creator" content={METADATA.twitter} />
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={TITLE} />
+        <meta name="twitter:image" content={METADATA.image} />
+
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={METADATA.url} />
+        <meta property="og:image" content={METADATA.image} />
+        <meta property="og:description" content={TITLE} />
+        <meta property="og:site_name" content={TITLE} />
+
+        <meta name="keywords" content={METADATA.keywords.join(", ")}></meta>
+
+        <link rel="icon" href={METADATA.favicon} />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.title}>HackTUES 9</h1>
+        <div>
+          <h1 className={styles.title}>{TITLE}</h1>
+        </div>
         <CardContainer />
       </main>
     </div>
   );
-}
+};
+
+export default Home;
