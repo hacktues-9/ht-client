00eca19d9935d520f1d2d1c221a3578355790ce7
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-01-10 15:10:20 +0200

    [FIX] Auth pages

---
+++ pages/users/[user].tsx
@@ -0,0 +1,57 @@
+import { useRouter } from "next/router";
+import { useAuthContext } from "../../context/authContext";
+import { isUser } from "../../utils/auth";
+
+const User = () => {
+  const router = useRouter();
+  const { user } = router.query as { user: string };
+  const { authState } = useAuthContext();
+
+  // check if user has right to see personal info
+
+  const {
+    name,
+    email,
+    phone,
+    sclass,
+    shirtSize,
+    emailVerified,
+    profilePic,
+    technologies,
+  } = {
+    name: "John Doe",
+    email: "john@elsys-bg.org",
+    phone: "0888888888",
+    sclass: "11B",
+    shirtSize: "M",
+    emailVerified: true,
+    profilePic: "https://i.imgur.com/6jJZj0L.jpg",
+    technologies: ["C++", "Java", "Python", "JavaScript", "HTML"],
+  }; // get from api with refresh token and change it
+
+  if (isUser(authState.userId, user))
+    return (
+      <div>
+        <h1>{name}</h1>
+        <p>{email}</p>
+        <p>{phone}</p>
+        <p>{sclass}</p>
+        <p>{shirtSize}</p>
+        <p>{emailVerified}</p>
+        <img
+          src={profilePic}
+          alt="profile picture"
+          style={{ width: "100px", height: "100px" }}
+        />
+        <ul>
+          {technologies.map((tech) => (
+            <li>{tech}</li>
+          ))}
+        </ul>
+      </div>
+    );
+
+  return <div>Profile {user}</div>;
+};
+
+export default User;
