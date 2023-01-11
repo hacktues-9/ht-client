7f7aa2194d0d40a7f2afa928d5b48ff01fe97c62
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-01-11 16:30:49 +0200

    [ADD] Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
