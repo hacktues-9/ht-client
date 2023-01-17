268bb2495a7e1aef021503059472b3c27c1213c1
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-01-17 18:12:50 +0200

    [UPDATE] Page Extentions - next config + file ext

--- next.config.js
+++ next.config.js
@@ -19,6 +19,8 @@ const nextConfig = {
     locales: ["bg", /*"en"*/],
     defaultLocale: "bg",
   },
+  // valid page extensions
+  pageExtensions: ["page.tsx"],
 };
 
 module.exports = nextConfig;
