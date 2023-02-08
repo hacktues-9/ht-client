2933780c9f77f09cb8cad7fe8fed658e9ee22148
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-02-08 12:43:47 +0200

    [ADD] Our team video

--- pages/ourteam/index.page.tsx
+++ pages/ourteam/index.page.tsx
@@ -1,5 +1,21 @@
+import style from "./style.module.scss";
+
 const HTTeam = () => {
-  return <h1>снимки скоро</h1>;
+  return (
+    <div className={style.page}>
+      <div className={style.container}>
+        <iframe
+          width="1920"
+          height="1080"
+          src="https://www.youtube-nocookie.com/embed/Ph79htwZlkw"
+          title="нашият екип"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default HTTeam;
