7209a73d931019c9a33631f2fc933d38cae775f0
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-02-11 23:53:38 +0200

    [UPDATE] Sponsors

--- partials/home/Sponsors.tsx
+++ partials/home/Sponsors.tsx
@@ -70,15 +70,108 @@ const SPONSORS = [
   },
   {
     type: "бета спонсори",
-    sponsors: [{
-      name: "accedia",
-      logo: "/assets/sponsors/beta/accedia.png",
-      url: "https://accedia.com/",
-    }],
+    sponsors: [
+      {
+        name: "accedia",
+        logo: "/assets/sponsors/beta/accedia.png",
+        url: "https://accedia.com/",
+      },
+      {
+        name: "Amusnet",
+        logo: "/assets/sponsors/beta/amusnet.png",
+        url: "https://amusenet.com/",
+      },
+      {
+        name: "Chaos",
+        logo: "/assets/sponsors/beta/chaos.jpg",
+        url: "https://chaos.tech/",
+      },
+      {
+        name: "Dreamix",
+        logo: "/assets/sponsors/beta/dreamix.png",
+        url: "https://dreamix.bg/",
+      },
+      {
+        name: "Limechain",
+        logo: "/assets/sponsors/beta/limechain.png",
+        url: "https://limechain.tech/",
+      },
+      {
+        name: "MentorMate",
+        logo: "/assets/sponsors/beta/mentormate.svg",
+        url: "https://www.mentormate.com/",
+      },
+      {
+        name: "mm",
+        logo: "/assets/sponsors/beta/mm.png",
+        url: "https://www.mm-sol.com/",
+      },
+      {
+        name: "Neterra",
+        logo: "/assets/sponsors/beta/neterra.png",
+        url: "https://neterra.com/",
+      },
+      {
+        name: "PaySafe",
+        logo: "/assets/sponsors/beta/paysafe.png",
+        url: "https://www.paysafe.com/",
+      },
+      {
+        name: "Progress",
+        logo: "/assets/sponsors/beta/progress.png",
+        url: "https://www.progress.com/",
+      },
+      {
+        name: "Sirma",
+        logo: "/assets/sponsors/beta/sirma.png",
+        url: "https://sirma.bg/",
+      },
+      {
+        name: "Strypes",
+        logo: "/assets/sponsors/beta/strypes.png",
+        url: "https://strypes.com/",
+      },
+      {
+        name: "Yettel",
+        logo: "/assets/sponsors/beta/yettel.png",
+        url: "https://yettel.bg/",
+      }
+    ],
   },
   {
     type: "гама спонсори",
-    sponsors: [],
+    sponsors: [
+      {
+        name: "Astea",
+        logo: "/assets/sponsors/gamma/astea.svg",
+        url: "https://asteasolutions.com",
+      },
+      {
+        name: "cnsys",
+        logo: "/assets/sponsors/gamma/cnsys.png",
+        url: "https://www.cnsys.bg/",
+      },
+      {
+        name: "Devrix",
+        logo: "/assets/sponsors/gamma/devrix.png",
+        url: "https://devrix.com/",
+      },
+      {
+        name: "IBM",
+        logo: "/assets/sponsors/gamma/ibm.svg",
+        url: "https://www.ibm.com/",
+      },
+      {
+        name: "itgix",
+        logo: "/assets/sponsors/gamma/itgix.png",
+        url: "https://itgix.com/",
+      },
+      {
+        name: "Nemetcheck",
+        logo: "/assets/sponsors/gamma/nemetcheck.png",
+        url: "https://nemetcheck.com/",
+      },
+    ],
   },
 ];
 
