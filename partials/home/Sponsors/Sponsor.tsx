3ea9a57ed1a8a4e0d14fd8e4e7eb4b065f358547
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-02-12 14:49:01 +0200

    [UPDATE] Links to open in new tab

--- partials/home/Sponsors/Sponsor.tsx
+++ partials/home/Sponsors/Sponsor.tsx
@@ -3,9 +3,13 @@ import style from "../../../styles/Sponsors.module.scss";
 const Sponsor = ({ name, logo, url }) => {
   if (!logo || !url) return null;
 
+{
+  /* new tab  - open <a> in new tab */
+} 
+
   return (
     <li className={style.sponsor}>
-      <a href={url}>
+      <a href={url} target="_blank" rel="noreferrer">
         <img src={logo} alt={name}></img>
       </a>
     </li>
