b064ca817fec8457f12537ff2d59adcc77d39678
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-02-19 00:07:08 +0200

    [FIX] OOPsie

--- pages/forgotten-password/reset/[token].page.tsx
+++ pages/forgotten-password/reset/[token].page.tsx
@@ -56,7 +56,7 @@ const ResetPage = () => {
       })
       .catch((err) => {
         if (err.message === "The string did not match the expected pattern.") {
-          setError(false);
+          setError("");
           setSuccess("Успешно променихте паролата си! След 3 секунди ще бъдете пренасочени към страницата за вход.");
           // after 3 seconds redirect to login
           setTimeout(() => {
