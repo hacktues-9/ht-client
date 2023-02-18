46f4ef8093979dca981b9a4eaeca265789cf4ce3
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-02-18 22:20:01 +0200

    [ADD] Success

--- pages/forgotten-password/reset/[token].page.tsx
+++ pages/forgotten-password/reset/[token].page.tsx
@@ -55,6 +55,15 @@ const ResetPage = () => {
         setWorking(false);
       })
       .catch((err) => {
+        if (err.message === "The string did not match the expected pattern.") {
+          setError(false);
+          setSuccess("Успешно променихте паролата си! След 3 секунди ще бъдете пренасочени към страницата за вход.");
+          // after 3 seconds redirect to login
+          setTimeout(() => {
+            router.push("/login");
+          }, 3000);
+          return;
+        }
         setError(err.message);
         setWorking(false);
       });
