143a91564420e6588297d02ed1fba6117181b123
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-03-11 22:36:02 +0200

    [ADD] Idk

--- pages/rankings/index.page.tsx
+++ pages/rankings/index.page.tsx
@@ -1,92 +1,76 @@
-// 4:00 - 14:15    Exotic Whip
-// 14:18 - 14:33    Баладжинатор
-// 14:36 - 14:51    "team_name" undefined
-// 14:54 - 15:09    TtT
-// 15:12 - 15:27    443
-// 15:33 - 15:48    Лостаджиите
-// 15:51 - 16:06    False Positive
-// 16:12 - 16:27    Жълта картина с котка
-// почивка и кетъринг
-// 16:30 - 16:45    the chu
-// 16:48 - 17:03    StanaGreshka
-// 17:06 - 17:21    113
-// 17:24 - 17:39    Екзотични
-// 17:42 - 17:57    4090
-// 18:00 - 18:15    AdvancedBegginersGuide
-
 import Link from "next/link";
 
-import style from "./style.module.scss";
 import Head from "next/head";
+import style from "./style.module.scss";
 
 const RANKINGS = [
   {
-    place: 0,
+    place: 12,
     name: "Exotic Whip",
     id: 86,
   },
   {
-    place: 0,
+    place: 3,
     name: "Баладжинатор",
     id: 40,
   },
   {
-    place: 0,
+    place: 14,
     name: '"team_name" undefined',
     id: 30,
   },
   {
-    place: 0,
+    place: 8,
     name: "TtT",
     id: 24,
   },
   {
-    place: 0,
+    place: 9,
     name: "443",
     id: 55,
   },
   {
-    place: 0,
+    place: 13,
     name: "Лостаджиите",
     id: 48,
   },
   {
-    place: 0,
+    place: 2,
     name: "False Positive",
     id: 2,
   },
   {
-    place: 0,
+    place: 7,
     name: "Жълта картина с котка",
     id: 44,
   },
   {
-    place: 0,
+    place: 11,
     name: "the chu",
     id: 18,
   },
   {
-    place: 0,
+    place: 4,
     name: "StanaGreshka",
     id: 87,
   },
   {
-    place: 0,
+    place: 5,
     name: "113",
     id: 56,
   },
   {
-    place: 0,
+    place: 6,
     name: "Екзотични",
     id: 22,
   },
   {
-    place: 0,
+    place: 1,
     name: "4090",
     id: 11,
   },
   {
-    place: 0,
+    place: 10,
     name: "AdvancedBegginersGuide",
     id: 69,
   },
@@ -95,33 +79,41 @@ const RANKINGS = [
 const Rankings = () => {
   return (
     <>
-    <Head>
-      <title>Класация</title>
-    </Head>
+      <Head>
+        <title>Класация</title>
+      </Head>
       <div className={style.rankings}>
-        <h1 className={style.rankings_title}>Rankings</h1>
+        <h1 className={style.rankings_title}>Класация</h1>
         <div className={style.rankings_container}>
           <ul>
-            {RANKINGS.map((team) => (
-              <li key={team.id}>
-                <Link href={`/teams/${team.id}`}>
-                  <p>{team.name}</p>
-                  <p style={{
-                    backgroundColor: "rgba(255, 255, 255, 0.1)",
-                    padding: "0.5rem",
-                    width: "3rem",
-                    height: "3rem",
-                    aspectRatio: "1/1",
-                    textAlign: "center",
-                    borderRadius: "50%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    border: "1px solid rgba(255, 255, 255, 0.1)",
-                  }}>{team.place}</p>
-                </Link>
-              </li>
-            ))}
+            {/* sort by place  */}
+            {RANKINGS.sort((a, b) => a.place - b.place).map(
+              (team) =>
+                team.place !== 0 && (
+                  <li key={team.id}>
+                    <Link href={`/teams/${team.id}`}>
+                      <p>{team.name}</p>
+                      <p
+                        style={{
+                          backgroundColor: "rgba(255, 255, 255, 0.1)",
+                          padding: "0.5rem",
+                          width: "3rem",
+                          height: "3rem",
+                          aspectRatio: "1/1",
+                          textAlign: "center",
+                          borderRadius: "50%",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          border: "1px solid rgba(255, 255, 255, 0.1)",
+                        }}
+                      >
+                        {team.place}
+                      </p>
+                    </Link>
+                  </li>
+                )
+            )}
           </ul>
         </div>
       </div>
