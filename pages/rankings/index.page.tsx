a2f52a269310845412e865f5778cc4c5583c00ef
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-03-11 19:59:27 +0200

    [ADD] Border

--- pages/rankings/index.page.tsx
+++ pages/rankings/index.page.tsx
@@ -117,6 +117,7 @@ const Rankings = () => {
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
+                    border: "1px solid rgba(255, 255, 255, 0.1)",
                   }}>{team.place}</p>
                 </Link>
               </li>
